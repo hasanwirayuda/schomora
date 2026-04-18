945d84e9270428637628a1d34246305ef8d3f7ac
Hasan Wirayuda <hasanwirayuda375@gmail.com>
2026-04-19 02:31:52 +0700

    chore: disable dev tools (next + tanstack)

--- apps/web/providers/query-provider.tsx
+++ apps/web/providers/query-provider.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 
 export default function QueryProvider({
@@ -14,7 +13,7 @@ export default function QueryProvider({
       new QueryClient({
         defaultOptions: {
           queries: {
-            staleTime: 60 * 1000, // 1 menit
+            staleTime: 60 * 1000,
             retry: 1,
           },
         },
@@ -22,9 +21,6 @@ export default function QueryProvider({
   );
 
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
