b4d8c1e6239731713849aa9c9ab2ab879060fcaa
Hasan Wirayuda <hasanwirayuda375@gmail.com>
2026-04-26 21:44:09 +0700

    chore(api): update CORS backend to add vercel domain

--- apps/api/cmd/server/main.go
+++ apps/api/cmd/server/main.go
@@ -45,7 +45,7 @@ func main() {
     r := gin.Default()
 
     r.Use(cors.New(cors.Config{
-        AllowOrigins:     []string{"http://localhost:3000"},
+        AllowOrigins:     []string{"http://localhost:3000", "https://schomora.vercel.app"},
         AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
         AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
         AllowCredentials: true,
